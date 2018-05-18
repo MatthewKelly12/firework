ede288f738576a335762f30f325924da6450d9fe
Matthew Kelly <mkonabike@gmail.com>
2018-05-17 20:14:26 -0500

    started explosion

--- canvas.js
+++ canvas.js
@@ -5,8 +5,15 @@ canvas.height = window.innerHeight;
 
 let c = canvas.getContext("2d");
 
-c.beginPath);
-c.arc()
+let radius = 3;
+function cic ()  {
+    c.beginPath();
+    c.arc(500, y, 3, 0, Math.PI * 2);
+    c.stroke();
+    dy = 0;
+    radius +=1;
+}
+
 
 
 let y = innerHeight;
@@ -15,11 +22,15 @@ function animate () {
     requestAnimationFrame(animate);
     c.clearRect(0, 0, innerWidth, innerHeight);
     c.beginPath();
-    c.arc(500, y, 8, 0, Math.PI * 2);
-    c.fill();
+    c.arc(500, y, radius, 0, Math.PI * 2);
+    c.stroke();
+
     dy -= .3;
     y -= dy;
-
+    console.log(dy)
+    if (dy === 0 || dy < 0) {
+        cic();
+    }
 }
 
 animate();
